96ceca8e14fc65b785698e061b5841ec2e5f2105
mohamedmarzuq5 <mohamedmarzuq2005@gmail.com>
2022-07-04 20:28:03 +0530

    logout added

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,16 @@ const userHelpers = require('../helpers/user-helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+	let user = req.session.user;
+	console.log(user);
 	productHelpers.getAllProducts().then((products) => {
-		console.log(products);
-		res.render('user/view-products', { admin: false, products });
+		res.render('user/view-products', { admin: false, products, user });
 	});
 });
 
 router.get('/login', (req, res) => {
-	res.render('user/login');
+	let user = req.session.user;
+	res.render('user/login', { admin: false, user });
 });
 
 router.get('/signup', (req, res) => {
@@ -27,11 +29,18 @@ router.post('/signup', (req, res) => {
 
 router.post('/login', (req, res) => {
 	userHelpers.doLogin(req.body).then((response) => {
-		if(response.status) {
-			res.redirect('/')
+		if (response.status) {
+			req.session.loggedIn = true;
+			req.session.user = response.user;
+			res.redirect('/');
 		} else {
-			res.redirect('/login')
+			res.redirect('/login');
 		}
-	})
-})
+	});
+});
+
+router.get('/logout', (req, res) => {
+	req.session.destroy();
+	res.redirect('/');
+});
 module.exports = router;
